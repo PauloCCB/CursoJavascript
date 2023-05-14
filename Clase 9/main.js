a07cfe537cbbf61cee5e61ac1202a42676d51d25
Paulo Calderon <paulo.napasa3@gmail.com>
2023-05-13 23:11:50 -0500

    Segundo

---
+++ Clase 9/main.js
@@ -0,0 +1,12 @@
+//DOM : Documento object model
+//console.log(document.body);
+// let div = document.getElementById("aplicacion");
+//console.log(div.innerHTML); 
+//console.log(div.innerText);
+// let p=document.getElementById("parrafo");
+// console.log(p.innerHTML);
+//En javascript usamos el id, la clases son mas para CSS.
+
+//getElements
+
+//Creación de un nodo
